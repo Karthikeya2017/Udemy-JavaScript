762756a6450d6f869d68a4d1706ae9f05c9a64c4
C61353 <gopikrishna.pentyala@nl.abnamro.com>
2020-10-13 14:37:02 +0200

    completing loop challenge

--- Loops/challenge.js
+++ Loops/challenge.js
@@ -1,6 +1,12 @@
-let tipCost = [ ];
-let finalCost = [ ];
-let billCost = [124, 48, 268, 180, 42];
+// Mark's trip details
+let MarktipCost = [ ];
+let MarkfinalCost = [ ];
+let MarkbillCost = [77, 375, 110, 45];
+
+// Joph's trip details
+let JohntipCost= [ ];
+let JohnfinalCost = [ ];
+let JohnbillCost = [124, 48, 268, 180, 42];
 
 
 // this is function declaration.
@@ -18,11 +24,48 @@ function tipCalculator(cost) {
 // I did a mistake is that - in line 22 adding element to billcost array, which leads infinite loop and memory crash
 // Initally tried to declare tipamount variable which doesn't require to declare.
 
-for (let i =0; i < billCost.length; i++){
-    tipAmount = tipCalculator(billCost[i]);
-    tipCost.push(tipAmount);
-    finalCost.push( billCost[i] + tipAmount);
+function billCalculator(billArr, tipCost, finalCost){
+    for (let i =0; i < billArr.length; i++){
+        tipAmount = tipCalculator(billArr[i]);
+        tipCost.push(tipAmount);
+        finalCost.push( billArr[i] + tipAmount);
+    }
 };
 
-console.log(`tip amount is [${tipCost}]`);
-console.log(`total trip cost is [${finalCost}]`);
+billCalculator(MarkbillCost, MarktipCost, MarkfinalCost);
+billCalculator(JohnbillCost,JohntipCost, JohnfinalCost);
+
+console.log(`Mark tip costs are: [${MarktipCost}]`);
+console.log(`Mark total trip cost is [${MarkfinalCost}]`);
+console.log(`John tip costs are: [${JohntipCost}]`);
+console.log(`John total trip cost is [${JohnfinalCost}]`);
+
+// by using reduce method
+
+function average(tipArr) {
+    return tipArr.reduce((a, b) => (a + b)) / tipArr.length;
+}
+
+MarkTipAvg = average(MarktipCost);
+JohnTipAvg = average(JohntipCost);
+
+console.log(`Mark's family Avg tip cost: ${MarkTipAvg}`);
+console.log(`John's family Avg tip cost: ${JohnTipAvg}`);
+
+(MarkTipAvg > JohnTipAvg) ? console.log(`Mark paid high tip;`):console.log(`John paid high tip`);
+
+// without any methods
+function averageTripCal(tipArr){
+    // if we declare sum variable outside function, first array sum became sum for initial value for second array.
+    let sum = 0;
+    for (let i = 0; i < tipArr.length; i++) {
+         sum += tipArr[i];
+    }
+    return sum / tipArr.length;
+};
+
+MarkTipAvg1 = averageTripCal(MarktipCost);
+JohnTipAvg1 = averageTripCal(JohntipCost);
+
+console.log(`Mark's family Avg tip cost: ${MarkTipAvg1}`);
+console.log(`John's family Avg tip cost: ${JohnTipAvg1}`);
