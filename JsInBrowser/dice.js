66f9c49c9c7b71a3cfab22a8613422c1ed4924ce
C61353 <gopikrishna.pentyala@nl.abnamro.com>
2020-10-23 21:07:20 +0200

    adding rolldice function

--- JsInBrowser/dice.js
+++ JsInBrowser/dice.js
@@ -1,31 +1,48 @@
-let playerAscore = 0;
-let playerBscore = 0;
-let gameScore = 0;
-let number = 0;
-let hold = false;
-let currentPlayer = 'playerA';
+const prompt = require('prompt');
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  readline.question('Hey do you want pause the game?', name => {
-    hold = true;
-    readline.close();
-  });
+let playerScores = [0, 0];
+let activePlayer = 0;
+let score = 0;
+const hold = 'true';
+const dice = Math.floor((Math.random() * 6) + 1);
+prompt.start();
 
-if(gameScore < 100){
-        while(number !== 1){
-            gameScore = gameScore + number;
-            (currentPlayer === 'playerA')? playerAscore += gameScore : playerBscore += gameScore;
-            if(!hold){
-                (currentPlayer === 'playerA')? currentPlayer = 'playerB ': currentPlayer = 'playerA';
-            }
-            number = Math.floor((Math.random() * 6) + 1);
-            console.log(`number: ${number}`);
-            console.log(`gameScore: ${gameScore}`);
-            readline;
-        }
+// prompt.get(['rolldice'], function (err, result) {
+//     if (err) { return onErr(err); }
+//     console.log('Command-line input received:');
+//     console.log('rolldice: ' + result.rolldice);
+// });
+
+// function onErr(err) {
+//     console.log(err);
+//     return 1;
+// }
+
+function rollDice() {
+  const diceTurnValue = Math.floor((Math.random() * 6) + 1);;
+  console.log(`diceTurnValue: ${diceTurnValue}`);
+  if(diceTurnValue !== 1){
+    score += diceTurnValue;
+    if(hold === 'true'){
+      playerScores[activePlayer]+=score;
+      console.log(`active player is: ${activePlayer}`);
+      (activePlayer === 0)? activePlayer = 1: activePlayer =0;
+      score = 0;
+    }
+  }else{
+    (activePlayer === 0)? activePlayer = 1: activePlayer =0;
+    console.log(`${activePlayer} is became active player`);
+    score = 0;
+  }
+};
+
+console.log(playerScores);
+while (playerScores[0] < 100 && playerScores[1] < 100) {
+    rollDice()
 }
 
-console.log(currentPlayer);
+console.log(`active player: ${activePlayer}`);
+console.log(`First player score is : ${playerScores[0]}`);
+console.log(`Second player score is : ${playerScores[1]}`);
+console.log(playerScores);
+console.log(score);
